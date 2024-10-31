fa7c6340f826b35d7e6f1201a1fdeaa5757958e8
FelipePatroni <felipe.patroni@sptech.school>
2024-10-31 13:16:24 -0300

    quase pronto

--- src/main/java/school/sptech/Consultoria.java
+++ src/main/java/school/sptech/Consultoria.java
@@ -1,4 +1,121 @@
 package school.sptech;
 
+import school.sptech.especialistas.DesenvolvedorMobile;
+import school.sptech.especialistas.DesenvolvedorWeb;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Consultoria {
+    private String nome;
+    private Integer vagas;
+    private List<Desenvolvedor> desenvolvedores;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getVagas() {
+        return vagas;
+    }
+
+    public void setVagas(Integer vagas) {
+        this.vagas = vagas;
+    }
+
+    public Consultoria() {
+        desenvolvedores = new ArrayList<>();
+    }
+
+    public Consultoria(String nome, Integer vagas) {
+        this.nome = nome;
+        this.vagas = vagas;
+        desenvolvedores = new ArrayList<>();
+    }
+
+    public Boolean existePorNome (String nome) {
+        Integer trava = 0;
+        for (int i = 0; i < desenvolvedores.size(); i++) {
+            if (desenvolvedores.get(i).getNome().equals(nome)){
+                trava = 1;
+            }
+        }
+        if (trava == 0){
+            return false;
+        }
+        return true;
+    }
+
+    public void contratar (Desenvolvedor desenvolvedor){
+        if (desenvolvedores.size() < vagas){
+            desenvolvedores.add(desenvolvedor);
+        }
+    }
+
+    public void contratarFullstack (DesenvolvedorWeb desenvolvedor){
+        if (desenvolvedor.isFullstack()){
+            desenvolvedores.add(desenvolvedor);
+        }
+    }
+
+    public Integer qtdDesenvolvedoresMobile(){
+        Integer trava = 0;
+        for (int i = 0; i < desenvolvedores.size(); i++) {
+            if(desenvolvedores.get(i) instanceof DesenvolvedorMobile){
+                trava ++;
+            }
+        }
+        return trava;
+    }
+
+    public Double getTotalSalarios(){
+        Double parametro = 0.0;
+        for (int i = 0; i < desenvolvedores.size(); i++) {
+            parametro = parametro + desenvolvedores.get(i).calcularSalario();
+        }
+        return parametro;
+    }
+
+    public Desenvolvedor buscarMenorSalario(){
+        if(desenvolvedores.isEmpty()){
+            return null;
+        }
+        Double salarioBase = desenvolvedores.get(0).calcularSalario();
+        Desenvolvedor dev = desenvolvedores.get(0);
+        for (int i = 0; i < desenvolvedores.size(); i++) {
+            if (salarioBase > desenvolvedores.get(i).calcularSalario()){
+                salarioBase = desenvolvedores.get(i).calcularSalario();
+                dev = desenvolvedores.get(i);
+            }
+        }
+        return dev;
+    }
+
+    public List<Desenvolvedor> buscarPorSalarioMaiorIgualQue(Double salario){
+        List<Desenvolvedor> devCorreto = new ArrayList<>();
+        for (int i = 0; i < desenvolvedores.size(); i++) {
+            if (desenvolvedores.get(i).calcularSalario()>= salario){
+                devCorreto.add(desenvolvedores.get(i));
+            }
+        }
+        return devCorreto;
+    }
+
+
+
+    public Desenvolvedor getDevPorNome(String nome){
+        Desenvolvedor parametro = null;
+        for (int i = 0; i < desenvolvedores.size(); i++) {
+            if (desenvolvedores.get(i).getNome().equals(nome)){
+                parametro = desenvolvedores.get(i);
+            }
+        }
+        return parametro;
+    }
+
+
 }
--- src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
+++ src/main/java/school/sptech/especialistas/DesenvolvedorWeb.java
@@ -1,4 +1,63 @@
 package school.sptech.especialistas;
 
-public class DesenvolvedorWeb {
+import school.sptech.Desenvolvedor;
+
+public class DesenvolvedorWeb extends Desenvolvedor {
+    private String backend;
+    private String frontend;
+    private String sgbd;
+    private Integer horasMentoria;
+
+    public DesenvolvedorWeb() {
+    }
+
+    public String getBackend() {
+        return backend;
+    }
+
+    public void setBackend(String backend) {
+        this.backend = backend;
+    }
+
+    public String getFrontend() {
+        return frontend;
+    }
+
+    public void setFrontend(String frontend) {
+        this.frontend = frontend;
+    }
+
+    public String getSgbd() {
+        return sgbd;
+    }
+
+    public void setSgbd(String sgbd) {
+        this.sgbd = sgbd;
+    }
+
+    public Integer getHorasMentoria() {
+        return horasMentoria;
+    }
+
+    public void setHorasMentoria(Integer horasMentoria) {
+        this.horasMentoria = horasMentoria;
+    }
+
+    @Override
+    public Double calcularSalario() {
+        return (horasMentoria * 300) + (qtdHoras * valorHora);
+    }
+
+    public Boolean isFullstack(){
+       if(backend == null || backend.isBlank() ){
+           return false;
+       }
+       if (frontend == null || frontend.isBlank()){
+           return false;
+       }
+       if (sgbd == null || sgbd.isBlank()){
+           return false;
+       }
+        return true;
+    }
 }
